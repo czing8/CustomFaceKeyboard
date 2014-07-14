2b1d3b13ccdd2761bbcbac7513bb6641f03229df
Vols <zhengcan@keegoo.com>
2014-07-14 12:57:12 +0800

    Start Code

---
+++ CustomFaceKeyboard/CustomFaceKeyboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomFaceKeyboard
+//
+//  Created by Vols on 14-7-14.
+//  Copyright (c) 2014年 vols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
